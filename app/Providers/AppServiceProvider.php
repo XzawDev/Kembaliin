7a0a7023e57caed4410b569848afe2a1f47a1e31
XzawDev <alfanoesa@gmail.com>
2026-04-02 15:41:47 +0700

    Fix AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        if (app()->environment('production') || env('FORCE_HTTPS', false)) {
+        URL::forceScheme('https');
+    }
     }
 
     /**
